56d6b57810d186bf53af646d2c71ac63a3a38d24
gon <54636165+khonsaloh@users.noreply.github.com>
2021-05-09 13:04:45 +0200

    20210509130444

--- .local/bin/pantallazo.sh
+++ .local/bin/pantallazo.sh
@@ -2,14 +2,20 @@
 
 tiempo=$(date +%Y%m%d%H%M%S)
 
+dir=$HOME/mm/
+case $1 in
+	'fullscr') notify-send -t 1000 "pantallazo en 2 segundos" \
+		; scrot --delay 2 $dir$tiempo.png;;
+	'regscr') import $dir$tiempo.png ;;
+esac
 a=$(fzf --reverse --cycle << EOF
 pantalla completa
 region de pantalla
 EOF
 )
 [ -z "$a" ] && exit
-dir=$HOME/mm/
 case $a in
-	'pantalla completa') notify-send -t 1000 "pantallazo en 2 segundos"; scrot --delay 2 $dir$tiempo.png;;
+	'pantalla completa') notify-send -t 1000 "pantallazo en 2 segundos" \
+		; scrot --delay 2 $dir$tiempo.png;;
 	'region de pantalla') import $dir$tiempo.png ;;
 esac

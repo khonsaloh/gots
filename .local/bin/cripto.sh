b53e6e1ea51eebbd2ced0a3de227b9a122cb9430
gon <54636165+khonsaloh@users.noreply.github.com>
2021-05-15 05:02:20 +0200

    20210515050220

--- .local/bin/cripto.sh
+++ .local/bin/cripto.sh
@@ -3,17 +3,17 @@
 if [ -f "$1" ]; then
 	tipo="$(file -b --mime-type "$1" | rev| cut -d' ' -f1 | rev)"
 	case $tipo in
-	  application/pg*) gpg -d "$1";; #desncripta desde binario
-	  *) gpg -ca "$1";; # encripta
+	  application/pg*) gpg -d "$1";;
+	  *) gpg -ca "$1";;
 	esac
 else
-	a=$(pwd)
+	#a=$(pwd)
 	b="$(file --mime-type * | grep 'application/pgp' | fzf)" \
 		&& name=$(echo "$b" | cut -d' ' -f1 | tr -d ':') \
 		&& tipo=$(echo "$b" | rev | cut -d' ' -f1|rev)
 	[ -z "$b" ] && exit
 		case $tipo in
-	  		application/pg*) gpg -d "$name";; #desncripta desde binario
-	  		*) gpg -ca "$a/$b";; # encripta
+	  		application/pg*) gpg -d "$name";;
+	  		*) gpg -ca "$a/$b";;
 		esac
 fi
